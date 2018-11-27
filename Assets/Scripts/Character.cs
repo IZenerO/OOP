e840483dfbab6c0c9de53ed834c2f139f4e689fd
IZenerO <45403164+IZenerO@users.noreply.github.com>
2018-11-27 22:33:23 +0200

    Add project

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class Character : Unit {
+
+    [SerializeField]
+    private float speed = 3.0F;
+    [SerializeField]
+    private float lives = 5;
+    [SerializeField]
+    private float jumpForce = 100.0F;
+    [SerializeField]
+    
+
+    private int _prevTimeStamp = 0;
+    
+    [SerializeField]
+    private Bullet bullet;
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private bool isGrounded = false;
+   
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+    private void FixedUpdate()
+    {
+     CheckGround();
+    }
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (isGrounded) State = CharState.Idle;
+        if (Input.GetButtonDown("Fire1")) Shoot();
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButton("Jump")) Jump();
+    }
+
+   private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0F;
+
+        if (isGrounded) State = CharState.Run;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        
+    }
+
+    private void Shoot()
+    {
+        Vector3 position = transform.position; position.y += 0.8F; //position.x += -0.5F;
+        Bullet newBullet = Instantiate(bullet, position, bullet.transform.rotation) as Bullet;
+
+        newBullet.Parent = gameObject;
+        newBullet.Direction = newBullet.transform.right * (sprite.flipX ? -1.0F : 1.0F);
+
+    }
+
+    public override void ReceiveDamage()
+    {
+        var timeStamp = Environment.TickCount;
+        _prevTimeStamp = _prevTimeStamp == 0 ? timeStamp : _prevTimeStamp;
+
+        if (TimeSpan.FromTicks(timeStamp - _prevTimeStamp).TotalMilliseconds < 0.1F && _prevTimeStamp != timeStamp)// где 2000, миллисекунды, или же 2 секунды
+        {
+            return;
+        }
+        
+        _prevTimeStamp = timeStamp;
+
+        lives--;
+        Debug.Log(lives);
+        
+  
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(transform.up * 9.0F, ForceMode2D.Impulse);
+        rigidbody.AddForce(transform.right * (sprite.flipX ? -1.0F : 1.0F) * speed * -9.0F, ForceMode2D.Impulse);
+
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+
+        isGrounded = colliders.Length > 1;
+        
+        if (!isGrounded) State = CharState.Jump;
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Unit unit = collider.gameObject.GetComponent<Unit>();
+        if (unit) ReceiveDamage();
+        if (lives == 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+}
+
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump,
+}
---
+++ Assets/Scripts/DefaultMonster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultMonster : Unit
+{
+    [SerializeField]
+    private float lives = 2.0F;
+
+    protected virtual void Awake() { }
+    protected virtual void Start() { }
+    protected virtual void Update() { }
+    public override void ReceiveDamage()
+    {
+        lives--;
+        if (lives == 0) Die();
+       
+    }
+    protected virtual void OnTriggerEnter2D(Collider2D collider)
+    {
+        Bullet bullet = collider.GetComponent<Bullet>();
+
+        if (bullet)
+        {
+            ReceiveDamage();
+        }
+    }
+	
+}
